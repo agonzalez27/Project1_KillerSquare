6af3b68f8f9bcb7ad8bd2a42d7573607f94f770c
Andrew Gonzalez <andrew.gonzalez@gmail.com>
2016-10-09 21:06:24 -0700

    tried to end the game but was unsuccessful

--- js/main.js
+++ js/main.js
@@ -3,17 +3,35 @@ var canvas, canvasContext;
 var squareX = randomNumber();
 var squareY = 0;
 var squareXspeed = 3;
-var start = document.getElementById("start").addEventListener("click", startGame)
+var start = document.getElementById("start").addEventListener("click", startGame);
+var scoreBoard
+var level
+var canvas = document.getElementById("myCanvas");
+var canvasContext = canvas.getContext("2d");
+var framesPerSecond = 30;
 
+//creates animation by updating canvas 30 times a second.
 function startGame() {
-  canvas = document.getElementById("myCanvas");
-  canvasContext = canvas.getContext("2d");
-
-  var framesPerSecond = 30;
+  framesPerSecond;
   setInterval(updateAll, 1000/framesPerSecond);
 }
 
 function updateAll() {
+
+drawCanvas();
+drawSquare();
+
+}
+
+//draws canvas
+function drawCanvas() {
+  canvasContext.fillStyle = "black"
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+}
+
+//draws killer square and sets speed
+function drawSquare() {
   squareX += squareXspeed;
   squareY++
 
@@ -25,12 +43,18 @@ function updateAll() {
     squareXspeed *= -1
   }
 
-  canvasContext.fillStyle = "black"
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-
   canvasContext.fillStyle = "red"
   canvasContext.beginPath();
   canvasContext.fillRect(squareX, squareY, 50, 50)
+}
+
+//create & track score
+//update level
+//end game
+function gameOver() {
+  // if(squareY > 400) {
+  //   framesPerSecond = 0;
+  // }
 }
 
 //creates a random number between 1 and 650 and is plugged into squareX
